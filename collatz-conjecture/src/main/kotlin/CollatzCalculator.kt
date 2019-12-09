a4bad4ce35ecfb2d551e0a0eff1387670e5763da
salvescoding <salves2383@gmail.com>
2019-12-09 21:36:51 +0100

    refactor function to more functional approach

--- collatz-conjecture/src/main/kotlin/CollatzCalculator.kt
+++ collatz-conjecture/src/main/kotlin/CollatzCalculator.kt
@@ -1,20 +1,17 @@
-import java.lang.IllegalArgumentException
-
 class CollatzCalculator {
 
     companion object {
 
-        fun computeStepCount(input: Int, counter: Int = 0): Int {
+        fun computeStepCount(input: Int): Int {
             require(input > 0) {"Only natural numbers are allowed" }
-            if (input == 1) return counter
-            var newCounter = counter
-            return if (input.rem(2)  == 0) {
-                newCounter++
-                computeStepCount(input / 2, newCounter)
-            } else {
-                newCounter++
-                computeStepCount(input * 3 + 1, newCounter)
+            fun computeStepCount(input: Int, counter: Int) : Int {
+                if (input == 1) return counter
+                return if (input.rem(2)  == 0)
+                    computeStepCount(input / 2, counter + 1)
+                else
+                    computeStepCount(input * 3 + 1, counter + 1)
             }
+            return computeStepCount(input, 0)
         }
 
     }
