f3870b4473229e17dfcdb27cf2baa0724d2163d9
salvescoding <salves2383@gmail.com>
2020-01-09 16:59:48 +0100

    Refactor Nucleotide

--- nucleotide-count/src/main/kotlin/NucleotideCount.kt
+++ nucleotide-count/src/main/kotlin/NucleotideCount.kt
@@ -1,20 +1,14 @@
-class Dna(val nucleotoids: String) {
-    private val listOfAllowed = listOf('A','C','G','T')
+class Dna(val sequence: String) {
 
-    init { require(nucleotoids.all { it in listOfAllowed }) }
-
+    init {
+        require( sequence.all { it in listOf('A','C','G','T') } )
+    }
 
     val nucleotideCounts: Map<Char, Int>
-        get() {
-            val mutableMap = mutableMapOf('A' to 0, 'C' to 0, 'G' to 0, 'T' to 0)
-            return if (nucleotoids.isEmpty()) {
-                mutableMap
-            } else {
-                nucleotoids.split("").asSequence().filterNot { it.isEmpty() }.map { it.single() }.forEach {
-                    mutableMap.merge(it, 1, Int::plus)
-                }
-                mutableMap
+        get() =
+             "ACGT".map { it to 0 }.toMap().let {
+                map ->
+                map + sequence.groupingBy { it }.eachCount()
             }
-        }
 
 }
