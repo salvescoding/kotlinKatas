95b41df8d1e0f5e22266c9e45418993062bc4b1a
salvescoding <salves2383@gmail.com>
2020-01-10 19:50:09 +0100

    Add solution for saddle points

--- saddle-points/src/main/kotlin/Matrix.kt
+++ saddle-points/src/main/kotlin/Matrix.kt
@@ -1,3 +1,21 @@
-class Matrix {
+class Matrix(val grid: List<List<Int>>) {
+
+    val saddlePoints : Set<MatrixCoordinate>
+        get() {
+            val setOfMatrix = mutableSetOf<MatrixCoordinate>()
+            if (grid.isEmpty()) return emptySet()
+             grid.forEachIndexed { indexRow, list ->
+                list.forEachIndexed { indexColumn, i ->
+                    when {
+                        isHigherInTheRowAndSmallerOfTheColumn(i, list, indexColumn) -> setOfMatrix.add(MatrixCoordinate(indexRow + 1, indexColumn + 1))
+                    }
+                }
+            }
+            return setOfMatrix
+        }
+
+    private fun isHigherInTheRowAndSmallerOfTheColumn(i: Int, list: List<Int>, indexColumn: Int) =
+            i == list.max() && grid.all { list -> i <= list[indexColumn] }
+
 
 }
