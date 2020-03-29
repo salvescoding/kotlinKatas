45fab7e3fa7e97e260ae022539360b62d6a93c8a
salvescoding <salves2383@gmail.com>
2020-03-29 15:52:42 +0200

    Refactor bank account to use AtomicInteger and AtomicBoolean to handle concurrency

--- bank-account/src/main/kotlin/BankAccount.kt
+++ bank-account/src/main/kotlin/BankAccount.kt
@@ -1,25 +1,18 @@
+import java.util.concurrent.atomic.AtomicBoolean
+import java.util.concurrent.atomic.AtomicInteger
+
 class BankAccount {
 
-    var accountActive: Boolean = true
-        private set
+    private val accountActive: AtomicBoolean = AtomicBoolean(true)
+    private val _balance: AtomicInteger = AtomicInteger(0)
 
+    val balance: Int
+        get() { check(accountActive.get()); return _balance.get() }
 
-    var balance: Int = 0
-        get() { return if (accountActive) field else throw IllegalStateException("Account is closed") }
-        private set
-
-    @Synchronized fun adjustBalance(amount: Long) {
-        synchronized(balance) {
-            balance +=  when {
-                amount.toInt() < 0 && balance > amount.toInt() -> amount.toInt()
-                amount.toInt() > 0 -> amount.toInt()
-                else -> throw IllegalStateException("Not enough funds")
-            }
-        }
+    fun adjustBalance(amount: Int) {
+        check(accountActive.get())
+        _balance.addAndGet(amount)
     }
 
-
-    fun close() {
-        accountActive = false
-    }
+    fun close() = accountActive.set(false)
 }
